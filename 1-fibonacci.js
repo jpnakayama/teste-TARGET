6e80b6461c8fa8101ee40edd833f4ec9ee5bdf25
JPNAKAYAMA <jpn_rufino@hotmail.com>
2023-02-28 09:38:31 -0300

    teste 1 concluído - sequencia de fibonacci

---
+++ 1-fibonacci.js
@@ -0,0 +1,16 @@
+function fazParteSequenciaFibonacci(n) {
+  //Criação de duas variáveis que irão se somar em loop
+  //A variável "fib" irá sempre ser um valor que pertence à sequência de Fibonacci
+  let fibAnterior = 0
+  let fibAtual = 1
+
+  //A criação da variável temporária (fibTemp) serve para que o número atual da sequência de Fibonacci seja armazenado para o próximo loop em que ele terá o valor de fibAnterior
+  while (fibAtual < n) {
+    let fibTemp = fibAtual
+    fibAtual = fibAnterior + fibAtual
+    fibAnterior = fibTemp
+  }
+
+  //Retornar se 
+  return fibAtual === n
+}
